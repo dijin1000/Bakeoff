535c00567077839bbe415ef3baa335458d406365
E.J <egon_janssen@live.nl>
2020-10-16 18:02:19 +0200

    Updated the data controller to have the type of ingredient and adjusted the mutation and crossover operators

--- Bakeoff/Bakeoff/Bakeoff.py
+++ Bakeoff/Bakeoff/Bakeoff.py
@@ -1,22 +1,35 @@
 import datacontroller as dc
 import matplotlib.pyplot as plt
+import random as rnd
+import collections
+from datacontroller import data as data
+
+#the types of ingredients we define
+types = [
+    "Sour",       #0
+    "Water",      #1
+    "Sweetness",  #2
+    "Flour",      #3
+    "Spice",      #4
+    "Fluid",      #5
+]
 
 #example defining new recipy
-if(False):
+if(True):
     dc.define_whole_new_recipy(
         "beef chilli",
         [
-            [150,"g","minced beef"],
-            [100,"g","onion"],
-            [100,"ml","water"],
-            [300,"g","tomato"],
-            [5,"g","chilli powder"],
-            [300,"g","red kidney beans"]
+            [150,"g","minced beef",0],
+            [100,"g","onion", 0],
+            [100,"ml","water", 1],
+            [300,"g","tomato", 2],
+            [5,"g","chilli powder", 3],
+            [300,"g","red kidney beans", 4]
         ])
     dc.saveData()
 
 #configuration settings
-_mu = 10
+_mu = 1
 _lambda = 10
 _steps = 10000
 _convergence = 0.000001
@@ -25,8 +38,19 @@ _crossoverRate = 0.01
 #Create the initial population according to the (parent)population size (_mu)
 def createPopulation():
     global generation
+    global dict 
 
-    generation = random.choices(data,k=_mu)
+    #generation = rnd.random.choices(data,k=_mu)
+    generation = [(data["beef chilli"], 1)]
+
+
+    zeta = [x for recipy in list(data.values()) for x in recipy["ingredients"]]
+
+    dict = collections.defaultdict()
+
+    for ingredient in zeta:
+        dict.setdefault(ingredient["type"], []).append(ingredient) 
+
     return
 
 #Wheel selection on parents, better parents have more chance to be having a child then others.
@@ -35,10 +59,10 @@ def parentSelection():
 
     generation.sort(key=lambda x:x[1])
 
-    max = sum(generation)[1]
+    max = sum(fit for _, fit in generation)
 
-    idx = random()
-    idx2 = random()
+    idx = rnd.random()
+    idx2 = rnd.random()
 
     idx3 = 0
 
@@ -51,27 +75,53 @@ def parentSelection():
         idx2 -= generation[idx3][1]/max
         idx3 += 1
 
-    return (r1,r2)
+    return (r1[0],r2[0])
 
 #The crossover operator on two individuals holding only one child.
 def crossoverOperator(r1,r2):
     print("TODO")
-    for i in range():
-        if(random() > _crossoverRate):
-            r1[i] = r2[i]
-    return r1
+
+    r1_unique_idenitifier = list(set(map(lambda d: d['type'], r1["ingredients"])))
+    r2_unique_idenitifier = list(set(map(lambda d: d['type'], r2["ingredients"])))
+
+    unique_idenitifiers = r1_unique_idenitifier
+
+    copy = r1["ingredients"]
+
+    for idx in range(len(copy)):
+        if(rnd.random() < _crossover):
+
+            possibles = list(filter(lambda person: person['type'] == ingredient['type'], r2["ingredients"]))
+            if(len(possibles) != 0):
+                index = rnd.randint(0,len(possibles))
+                copy[idx] = possibles[idx]
+
+    return copy
+
+#
+def convertion(type,ingredient):
+    global dict
+
+    list = dict[type]
+    list.remove(ingredient)
+
+    idx = rnd.randint(0,len(list))
+
+    return list[idx]
 
 #The mutating operator that mutates a single individual.
 def mutationOperator(r1):
     print("TODO")
-    for i in range():
-        if(random() > _crossoverRate):
-            r1[i] = r1[i] + 1
+    for i in range(len(r1["ingredients"])):
+        if(rnd.random() < _mutation_type):
+            r1["ingredients"][i] = convertion(r1["ingredients"][i]["type"])
+        if(rnd.random() < _mutation_amount):
+            r1["ingredients"][i]["amount"] += rnd.randint(-mutation_power,mutation_power)
     return r1
 
 #The selection opeartor that picks the mu best individuals in a group of individuals
 def selectionOperator(individuals):
-    individuals.sort(key=lambda x:x[1])
+    individuals.sort(key=lambda tuple:tuple[1])
     return individuals[0:_mu]
 
 #The normalization operator to ensure that the recipies are correct and feasiable.
@@ -91,7 +141,7 @@ def geneticStep():
     children = []
 
     for i in range(_lambda):
-        r1, r2 = parentSelection(generation)
+        r1, r2 = parentSelection()
         r1 = crossoverOperator(r1,r2)
         r1 = mutationOperator(r1)
         r1 = normalise(r1)
@@ -106,8 +156,9 @@ def printRecipy(recipy):
     print("TODO")
     return
 
+
 #The complete program
-loadData()
+dc.loadData()
 
 createPopulation()
 
@@ -119,7 +170,7 @@ min_fitnesses = []
 
 global best_found_solution
 
-while(currentsteps < _steps and currentconvergence < _convergence):
+while(currentsteps < _steps and currentconvergence > _convergence):
     geneticStep()
 
     best = max(generation)[1]
--- Bakeoff/Bakeoff/datacontroller.py
+++ Bakeoff/Bakeoff/datacontroller.py
@@ -1,4 +1,5 @@
 import json
+from enum import Enum
 
 global data
 data = {}
@@ -6,13 +7,14 @@ data = {}
 #load the current data
 def loadData():
     global data 
-    data = json.load("data.txt")
+    with open('data.json', 'r') as readfile:
+        data = json.loads(readfile.read())
 
 #completly overwrite the previous data with the current data
 def saveData():
     global data 
-    with open('data.txt', 'w') as outfile:
-        json.dump(data, outfile,indent=2,sort_keys=False)
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile,indent=2,sort_keys=False,default=str)
 
 #print the current data
 def showData():
@@ -23,27 +25,28 @@ def showData():
 def define_whole_new_recipy(name,ingredients):
     define_new_recipy(name)
     for ingredient in ingredients:
-        add_ingredients_to_recipy(name,ingredient[1],ingredient[2],ingredient[0])
+        add_ingredients_to_recipy(name,ingredient[1],ingredient[2],ingredient[3],ingredient[0])
 
 #create a new recipy with only a name
 def define_new_recipy(name):
     data[name] = dataContainer()
 
 #add ingredients to a already excisting recipy
-def add_ingredients_to_recipy(name,ingredient_unit,ingredient_name,ingredient_amount=1):
-    data[name].add_ingredient(ingredient_unit,ingredient_name,ingredient_amount)
+def add_ingredients_to_recipy(name,ingredient_unit,ingredient_name,ingredient_type,ingredient_amount=1):
+    data[name].add_ingredient(ingredient_unit,ingredient_name,ingredient_type,ingredient_amount)
 
 #recipy container
 class dataContainer(dict):
     def __init__(self):
         self["ingredients"] = list()
     
-    def add_ingredient(self,ingredient_unit,ingredient_name,ingredient_amount=1):
-        self["ingredients"].append(ingredient(ingredient_amount,ingredient_unit,ingredient_name))
+    def add_ingredient(self,ingredient_unit,ingredient_name,ingredient_type,ingredient_amount=1):
+        self["ingredients"].append(ingredient(ingredient_amount,ingredient_unit,ingredient_name,ingredient_type))
 
 #ingredients container
 class ingredient(dict):
-    def __init__(self,_amount,_unit,_name):
+    def __init__(self,_amount,_unit,_name,_type):
         self["amount"] =_amount
         self["unit"] = _unit
         self["name"] = _name
+        self["type"] = _type
